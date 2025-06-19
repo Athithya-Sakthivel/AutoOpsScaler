93b6066abb1434e4b31491d0d727b8ecc230afc8
Athithya-Sakthivel <athithyasakthivel1@gmail.com>
2025-06-19 09:23:58 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -168,21 +168,6 @@ install_node_vite
 install_python
 mkdir -p tmp
 
-PLUGIN_NAME=aws
-PLUGIN_VERSION=6.44.0
-PLUGIN_ARCHIVE=pulumi-resource-${PLUGIN_NAME}-v${PLUGIN_VERSION}-linux-amd64.tar.gz
-PLUGIN_URL=https://github.com/pulumi/pulumi-${PLUGIN_NAME}/releases/download/v${PLUGIN_VERSION}/${PLUGIN_ARCHIVE}
-
-mkdir -p ~/.pulumi/plugins/resource-${PLUGIN_NAME}-v${PLUGIN_VERSION}
-
-echo "[INFO] Downloading ${PLUGIN_NAME} plugin v${PLUGIN_VERSION}..."
-wget -q --show-progress "$PLUGIN_URL"
-
-echo "[INFO] Extracting plugin..."
-tar -xzf "$PLUGIN_ARCHIVE" -C ~/.pulumi/plugins/resource-${PLUGIN_NAME}-v${PLUGIN_VERSION}/
-
-echo "[INFO] Cleaning up..."
-rm "$PLUGIN_ARCHIVE"
 
 echo "[✓] All tools installed & pinned with zero interactive prompts."
 echo "→ Open a new terminal or run 'source ~/.bashrc' to use ${PYTHON_VERSION} by default."
